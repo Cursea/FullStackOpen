04f5cc422014bd9f1e8deb57cc2ccca459817ae2
Marco <marcoburman@gmail.com>
2019-08-22 16:52:08 +0100

    add 1.12*: anecdotes step1

---
+++ part1/anecdotes/src/index.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+const App = (props) => {
+    const [selected, setSelected] = useState(0)
+    const min = 0
+    const max = anecdotes.length - 1
+    const setToValue = (set, min, max) => () => {
+        set(Math.floor(Math.random() * (max - min + 1) + min))
+    }
+
+    return (
+        <div>
+            {props.anecdotes[selected]}
+            <Button random={setToValue(setSelected, min, max)} text='next anecdote' />
+        </div>
+    )
+}
+
+const Button = ({ random, text }) => {
+
+    return (
+        <button onClick={random}>
+            {text}
+        </button>
+    )
+}
+
+const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+ReactDOM.render(
+    <App anecdotes={anecdotes} />,
+    document.getElementById('root')
+)
