53da61d2b1809fa505fda183a85f093226b1e90e
Marco <marcoburman@gmail.com>
2019-09-18 17:34:41 +0100

    initial 2.10: The Phonebook Step5
    ToDo: fix filter

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,73 +2,38 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      id: 1,
-      name: 'Arto Hellas',
-      number: '07986504758'
-    }
+    { id: 1, name: 'Arto Hellas', number: '07986504758' },
+    { id: 2, name: 'Ada Lovelace', number: '39-44-5323523' },
+    { id: 3, name: 'Dan Abramov', number: '12-43-234345' },
+    { id: 4, name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
 
-  const rows = () => persons.map(person =>
-    <div key={person.id}>{person.name} {person.number}</div>)
-
-  const addPerson = (event) => {
-    event.preventDefault()
-    const personObject = {
-      id: persons.length + 1,
-      name: newName,
-      number: newNumber
-    }
-
-    //check if person already exists in phonebook; search each key among persons objects for a match and returns Boolean
-    if (persons.some(e => e.name === newName)) {
-      window.alert(`${personObject.name} is already added to the phonebook`)
+/*
+  const numbersToShow = () => {
+    if (filter == '') {
+      rows()
     } else {
-      setPersons(persons.concat(personObject))
-      setNewName('')
-      setNewNumber('')
+      persons
+        .filter(person => person.name == filter)
+        .map(person =>
+          <div key={person.id}>{person.name} {person.number}</div>)
     }
-
   }
-
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
-
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
+  */
 
   return (
     <div>
       <h2>Phonebook</h2>
+
       <Filter />
+
       <h3>Add new entry</h3>
-      <form onSubmit={addPerson}>
-        <div>
-          <label htmlFor="name">Name: </label>
-          <input
-            value={newName}
-            onChange={handleNameChange}
-            id="name"
-          />
-        </div>
-        <div>
-          <label htmlFor="number">Number: </label>
-          <input
-            value={newNumber}
-            onChange={handleNumberChange}
-            id="number"
-          />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      {rows()}
+
+      <PersonForm persons={persons} setPersons={setPersons}/>
+
+      <h3>Numbers</h3>
+
+      <Persons persons={persons} />
     </div>
 
   )
@@ -91,6 +56,73 @@ const Filter = () => {
         id="filter"
       />
     </div>
+  )
+}
+
+const Persons = ({ persons }) => {
+
+  const rows = () => persons.map(person =>
+    <div key={person.id}>{person.name} {person.number}</div>)
+
+  return (
+    <div>
+      {rows()}
+    </div>
+  )
+}
+
+const PersonForm = ({persons, setPersons}) => {
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      id: persons.length + 1,
+      name: newName,
+      number: newNumber
+    }
+
+    //check if person already exists in phonebook; search each key among persons objects for a match and returns Boolean
+    if (persons.some(e => e.name === newName)) {
+      window.alert(`${personObject.name} is already added to the phonebook`)
+    } else {
+      setPersons(persons.concat(personObject))
+      setNewName('')
+      setNewNumber('')
+    }
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  return (
+    <form onSubmit={addPerson}>
+      <div>
+        <label htmlFor="name">Name: </label>
+        <input
+          value={newName}
+          onChange={handleNameChange}
+          id="name"
+        />
+      </div>
+      <div>
+        <label htmlFor="number">Number: </label>
+        <input
+          value={newNumber}
+          onChange={handleNumberChange}
+          id="number"
+        />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
   )
 }
 
