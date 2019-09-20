85245ac1020bbe78cf5f12b39c3f1adbd5a36649
Marco <marcoburman@gmail.com>
2019-09-20 03:03:39 +0100

    fix filter and begin refactor

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -7,44 +7,63 @@ const App = () => {
     { id: 3, name: 'Dan Abramov', number: '12-43-234345' },
     { id: 4, name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
-
-/*
-  const numbersToShow = () => {
-    if (filter == '') {
-      rows()
-    } else {
-      persons
-        .filter(person => person.name == filter)
-        .map(person =>
-          <div key={person.id}>{person.name} {person.number}</div>)
-    }
-  }
-  */
-
-  return (
-    <div>
-      <h2>Phonebook</h2>
-
-      <Filter />
-
-      <h3>Add new entry</h3>
-
-      <PersonForm persons={persons} setPersons={setPersons}/>
-
-      <h3>Numbers</h3>
-
-      <Persons persons={persons} />
-    </div>
-
-  )
-}
-
-const Filter = () => {
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
 
   const handleFilterChange = (event) => {
     setFilter(event.target.value)
   }
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      id: persons.length + 1,
+      name: newName,
+      number: newNumber
+    }
+    //check if person already exists in phonebook; search each key among persons objects for a match and returns Boolean
+    if (persons.some(e => e.name === newName)) {
+      window.alert(`${newName} is already added to the phonebook`)
+    } else {
+      setPersons(persons.concat(personObject))
+      setNewName('')
+      setNewNumber('')
+    }
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+
+      <Filter filter={filter} handleFilterChange={handleFilterChange} />
+
+      <h3>Add new entry</h3>
+
+      <PersonForm
+        addPerson={addPerson}
+        name={{ newName: newName, onChange: handleNameChange }}
+        number={{ newNumber: newNumber, onChange: handleNumberChange }}
+      />
+
+      <h3>Numbers</h3>
+
+      <Persons
+        persons={persons}
+        filter={filter}
+      />
+    </div>
+
+  )
+}
+
+const Filter = ({ filter, handleFilterChange }) => {
 
   return (
     <div>
@@ -59,63 +78,23 @@ const Filter = () => {
   )
 }
 
-const Persons = ({ persons }) => {
-
-  const rows = () => persons.map(person =>
-    <div key={person.id}>{person.name} {person.number}</div>)
-
-  return (
-    <div>
-      {rows()}
-    </div>
-  )
-}
-
-const PersonForm = ({persons, setPersons}) => {
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-
-  const addPerson = (event) => {
-    event.preventDefault()
-    const personObject = {
-      id: persons.length + 1,
-      name: newName,
-      number: newNumber
-    }
-
-    //check if person already exists in phonebook; search each key among persons objects for a match and returns Boolean
-    if (persons.some(e => e.name === newName)) {
-      window.alert(`${personObject.name} is already added to the phonebook`)
-    } else {
-      setPersons(persons.concat(personObject))
-      setNewName('')
-      setNewNumber('')
-    }
-  }
-
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
-
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
+const PersonForm = ({ addPerson, name, number }) => {
 
   return (
     <form onSubmit={addPerson}>
       <div>
         <label htmlFor="name">Name: </label>
         <input
-          value={newName}
-          onChange={handleNameChange}
+          value={name.newName}
+          onChange={name.onChange}
           id="name"
         />
       </div>
       <div>
         <label htmlFor="number">Number: </label>
         <input
-          value={newNumber}
-          onChange={handleNumberChange}
+          value={number.newNumber}
+          onChange={number.onChange}
           id="number"
         />
       </div>
@@ -123,6 +102,32 @@ const PersonForm = ({persons, setPersons}) => {
         <button type="submit">add</button>
       </div>
     </form>
+  )
+}
+
+const Persons = ({ persons, filter }) => {
+
+  const rows = () => persons.map(person =>
+    <div key={person.id}>{person.name} {person.number}</div>)
+
+  const numbersToShow = () => {
+    if (filter === '') {
+      return (rows())
+    } else {
+      return (
+        persons
+          .filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
+          .map(person =>
+            <div key={person.id}>{person.name} {person.number}</div>)
+        //above .map method on the filter duplicates rows() method..
+      )
+    }
+  }
+
+  return (
+    <div>
+      {numbersToShow()}
+    </div>
   )
 }
 
