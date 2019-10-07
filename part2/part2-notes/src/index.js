caddf8eeb953f3c5bce3146760410bb45d5936ec
Marco <marcoburman@gmail.com>
2019-10-07 17:31:53 +0100

    add part c: Effect-hooks

--- part2/part2-notes/src/index.js
+++ part2/part2-notes/src/index.js
@@ -3,24 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-import axios from 'axios';
-
-const promise = axios
-    .get('http://localhost:3001/notes')
-    .then(response => {
-        const notes = response.data
-        ReactDOM.render(
-            <App notes={notes} />,
-            document.getElementById('root')
-        )
-    })
-console.log(promise)
-
-
-const promise2 = axios.get('http://localhost:3001/foobar')
-console.log(promise2)
-
-
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
