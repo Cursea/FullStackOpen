1b2298221bc7789e0a3f3141ea06943797efe61e
Marco <marcoburman@gmail.com>
2019-10-14 03:36:15 +0100

    complete 2.12* Data for countries, step1 & add weather for step3

--- part2/data-for-countries/src/App.js
+++ part2/data-for-countries/src/App.js
@@ -8,11 +8,13 @@ function App() {
 
   const [filter, setFilter] = useState('')
   const [country, setCountry] = useState('')
+  const [weather, setWeather] = useState('')
 
   const handleFilterChange = (event) => {
     setFilter(event.target.value)
   }
 
+  //retrieve countries
   useEffect(() => {
     axios
       .get('https://restcountries.eu/rest/v2/all')
@@ -21,11 +23,21 @@ function App() {
       })
   }, [])
 
+  //retrieve weather
+  useEffect(() => {
+    axios
+      .get('http://api.weatherstack.com/current?access_key=4f8022b5b714698e39a011d2a53af96e&query=London')
+      .then(response => {
+        setWeather(response.data)
+      })
+  }, [])
+
+  console.log({ weather })
   return (
     <div>
       <Filter filter={filter} setFilter={handleFilterChange} />
-      {console.log(country)}
-      <Countries filter={filter} country={country}/>
+
+      <Countries filter={filter} country={country} weather={weather} />
     </div>
   )
 }
--- part2/data-for-countries/src/components/Countries.js
+++ part2/data-for-countries/src/components/Countries.js
@@ -1,22 +1,56 @@
 import React from 'react';
-import axios from '../../../part2-notes/node_modules/axios';
 
-const Countries = ({ country, filter }) => {
+const Countries = ({ country, filter, weather }) => {
 
-    const countryName = () => {
+    function commaSepNums(num) {
+        return (num + '').replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    }
+
+    const countryInfo = () => {
+        const gridParentStyle = {
+            display: 'grid',
+            gridTemplateColumns: '1fr 2fr 1fr'
+        }
+        const flagStyle = {
+            maxWidth: '600px',
+            maxHeight: '400px'
+        }
+
         if (filter == '') {
             return ''
         } else {
             return country
                 .filter(country => country.name.toLowerCase().includes(filter.toLowerCase()))
                 .map(country =>
-                    <div key={country.callingCodes}>{country.name}</div>)
+                    <section key={country.numericCode} style={gridParentStyle}>
+                        <div>
+                            <h1>{country.name}</h1>
+                            <p>capital: {country.capital}</p>
+                            <p>population: {commaSepNums(country.population)}</p>
+                            <p>area: {commaSepNums(country.area)}</p>
+                            <p>population density: {(country.population / country.area).toFixed(2)} per km<sup>2</sup></p>
+                            <h3>Langauges:</h3>
+                            <ul>
+                                {country.languages.map((lang, index) => <li key={index}>{lang.name}</li>)}
+                            </ul>
+                        </div>
+                        <div>
+                            <img src={country.flag} style={flagStyle} />
+                        </div>
+                        <div>
+                            <p>Weather in {weather.location.name}<br /> as of {weather.location.localtime}</p>
+                            <p>Temperature: {weather.current.temperature}°C</p>
+                            <p>Wind: {weather.current.wind_speed}kph direction {weather.current.wind_dir}</p>
+                            <img src={weather.current.weather_icons} />
+                        </div>
+                    </section>
+                )
         }
     }
 
     return (
         <div>
-            {countryName()}
+            {countryInfo()}
         </div>
     )
 }
