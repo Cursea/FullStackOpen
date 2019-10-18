42d06b0dfdb83168b0b5af9887ea21d20885c809
Marco <marcoburman@gmail.com>
2019-10-18 17:30:55 +0100

    begin refactoring app; reduce component size

--- part2/data-for-countries/src/App.js
+++ part2/data-for-countries/src/App.js
@@ -3,20 +3,12 @@ import './App.css';
 import axios from 'axios';
 import Filter from './components/Filter';
 import Countries from './components/Countries';
+import Weather from './components/Weather';
 
 function App() {
 
   const [filter, setFilter] = useState('')
   const [countries, setCountry] = useState('')
-  const [weather, setWeather] = useState('')
-
-  const showCountry = (name) => {
-    setFilter(name);
-  }
-
-  const handleFilterChange = (event) => {
-    setFilter(event.target.value)
-  }
 
   //retrieve countries; sets a massive JSON blob to 'country'
   useEffect(() => {
@@ -27,20 +19,21 @@ function App() {
       })
   }, [])
 
-  //retrieve weather
-  useEffect(() => {
-    axios
-      .get('http://api.weatherstack.com/current?access_key=4f8022b5b714698e39a011d2a53af96e&query=London') //http://api.weatherstack.com/current?access_key=4f8022b5b714698e39a011d2a53af96e&query=London
-      .then(response => {
-        setWeather(response.data)
-      })
-  }, [])
+  const showCountry = (name) => {
+    setFilter(name);
+  }
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
 
   return (
     <div>
       <Filter filter={filter} setFilter={handleFilterChange} />
 
-      <Countries filter={filter} countries={countries} weather={weather} showCountry={showCountry} />
+      <Countries filter={filter} countries={countries} showCountry={showCountry} />
+
+      <Weather capital={capital} />
     </div>
   )
 }
--- part2/data-for-countries/src/components/Countries.js
+++ part2/data-for-countries/src/components/Countries.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Countries = ({ countries, filter, weather, showCountry }) => {
+const Countries = ({ countries, filter, showCountry }) => {
 
     function commaSepNums(num) {
         return (num + '').replace(/\B(?=(\d{3})+(?!\d))/g, ',');
@@ -34,18 +34,9 @@ const Countries = ({ countries, filter, weather, showCountry }) => {
                     <div id="flag">
                         <img src={countries.flag} style={flagStyle} alt="national flag" />
                     </div>
-                    <div id="weather">
-                        <p>Weather in {weather.location.name}<br /> as of {weather.location.localtime}</p>
-                        <p>Temperature: {weather.current.temperature}°C</p>
-                        <p>Wind: {weather.current.wind_speed}kph direction {weather.current.wind_dir}</p>
-                        <img src={weather.current.weather_icons} alt="weather in capital city" />
-                    </div>
                 </section>
             )
         }
-
-        //WHHHHYYYYYYY!!!
-        //let results = countries.filter(country => country.name.toLowerCase().includes(filter.toLowerCase()));
 
         if (filter === '') {
             return
