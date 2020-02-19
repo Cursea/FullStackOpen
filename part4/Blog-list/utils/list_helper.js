43e673ddb4206831e175f11f07f2827c5880acf4
Marco <marcoburman@gmail.com>
2020-02-19 17:32:12 +0000

    Begin creating favouriteBlog function; add unit test for favouriteBlog

--- part4/Blog-list/utils/list_helper.js
+++ part4/Blog-list/utils/list_helper.js
@@ -10,7 +10,17 @@ const totalLikes = (blogs) => {
   return blogs.reduce(reducer, 0)
 }
 
+const favouriteBlog = (blogs) => {
+  const maxLikes = Math.max(...blogs.map(el => el.likes))
+  const best = blogs.map(blogs.find(blog => blog.likes  === maxLikes))
+
+  return {
+    f: 2
+  }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favouriteBlog
 }
