8c9577b42b79bb5baf4fe9371220d4351bf93d68
Marco <marcoburman@gmail.com>
2020-11-03 22:12:33 +0000

    Add 4.17: bloglist expansion, step5; make blog-user schemas cross-referential; specify schema return props

--- part4/Blog-list/controllers/blogs.js
+++ part4/Blog-list/controllers/blogs.js
@@ -1,19 +1,24 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
+
   response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response, next) => {
+blogsRouter.post('/', async (request, response) => {
   const body = request.body
+
+  const user = await User.findById(body.userId)
 
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes,
+    user: user._id,
   })
 
   if (!blog.title || !blog.url) {
@@ -21,12 +26,11 @@ blogsRouter.post('/', (request, response, next) => {
     return
   }
 
-  blog
-    .save()
-    .then((savedBlog) => {
-      response.status(201).json(savedBlog)
-    })
-    .catch((error) => next(error))
+  const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
+  response.json(savedBlog.toJSON())
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
